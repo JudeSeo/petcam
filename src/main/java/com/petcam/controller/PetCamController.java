f31f6aac801b6e93e3034d09f54cd3e563756df7
JudeSeo <gn0408@gmail.com>
2025-01-11 22:37:57 +0900

    connection 연결 해제 추가

--- src/main/java/com/petcam/controller/PetCamController.java
+++ src/main/java/com/petcam/controller/PetCamController.java
@@ -1,22 +1,29 @@
 package com.petcam.controller;
 
+import com.petcam.connection.SignalingServerHandler;
 import com.petcam.sevice.PetCamDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
 
 @RestController
 @RequestMapping("/PetCam")
 public class PetCamController {
+    @Autowired
+    SignalingServerHandler signalingServerHandler;
 
     @RequestMapping(value = "/connectPhone", method = RequestMethod.POST)
-    public ResponseEntity<Boolean> connectPhone(@RequestBody PetCamDto dto) {
+    public ResponseEntity<Boolean> connectPhone(@RequestBody PetCamDto dto, WebSocketSession session) {
         // 원격 핸드폰 연결
         try {
             dto.getDeviceId(); // 사용
             // 집에 있는 핸드폰과 연결
+            signalingServerHandler.afterConnectionEstablished(session);
         } catch (Exception e) {
             return ResponseEntity.ok(false);
         }
@@ -54,10 +61,11 @@ public class PetCamController {
     }
 
     @RequestMapping(value = "/stopCam", method = RequestMethod.POST)
-    public ResponseEntity<Boolean> stopCamera(@RequestBody PetCamDto dto) {
+    public ResponseEntity<Boolean> stopCamera(@RequestBody PetCamDto dto, WebSocketSession session, CloseStatus status) {
         // 원격 핸드폰에 명령 전송 로직 추가
         try {
             // 카메라 종료
+            signalingServerHandler.afterConnectionClosed(session, status);
         } catch (Exception e) {
             return ResponseEntity.ok(false);
         }
